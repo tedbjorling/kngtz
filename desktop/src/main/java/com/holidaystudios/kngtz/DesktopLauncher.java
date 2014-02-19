606a6a6d999fb93f1ee27ebed062b22a2aada8c2
tedbjorling <tedbjorling@gmail.com>
2014-02-19 21:53:45 +0100

    First commit from libgdx example

---
+++ desktop/src/main/java/com/holidaystudios/kngtz/DesktopLauncher.java
@@ -0,0 +1,13 @@
+
+package com.holidaystudios.kngtz;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.holidaystudios.kngt.HelloApp;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new HelloApp(), config);
+	}
+}
