5aab0c10e3456a2af006617e4d4cbced48fbaa90
tedbjorling <tedbjorling@gmail.com>
2014-02-21 23:05:55 +0100

    Implemented mouse/touch navigation

--- core/src/main/java/com/holidaystudios/kngt/screens/GameScreen.java
+++ core/src/main/java/com/holidaystudios/kngt/screens/GameScreen.java
@@ -2,6 +2,7 @@ package com.holidaystudios.kngt.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.OrthographicCamera;
@@ -11,10 +12,14 @@ import com.holidaystudios.kngt.KngtzMain;
 import com.holidaystudios.kngt.entities.Game;
 import com.holidaystudios.kngt.entities.Room;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
 /**
  * Created by tedbjorling on 2014-02-21.
  */
-public class GameScreen implements Screen {
+public class GameScreen implements Screen, InputProcessor {
 
     static final Integer TILE_DIM = 64;
     static final Integer VIEW_WIDTH  = 800;
@@ -31,6 +36,8 @@ public class GameScreen implements Screen {
     private Texture wallTexture;
     private Texture doorTexture;
 
+    private Set<Integer> pressedKeys = new HashSet<Integer>();
+    private Integer dragStartX, dragStartY = null;
 
     public GameScreen(final KngtzMain _kngtz) {
         kngtz = _kngtz;
@@ -45,11 +52,14 @@ public class GameScreen implements Screen {
 
         //Create game
         game = new Game();
+
+        Gdx.input.setInputProcessor(this);
     }
 
     @Override
     public void render(float delta) {
-        handleInput();
+
+        handlePressedKeys();
 
         GL10 gl = Gdx.graphics.getGL10();
 		gl.glClearColor(0, 0, 0, 1);
@@ -83,31 +93,6 @@ public class GameScreen implements Screen {
         kngtz.batch.end();
     }
 
-    private void handleInput() {
-
-        if(Gdx.input.isKeyPressed(Input.Keys.A)) {
-            camera.zoom += 0.1;
-            System.out.println(camera.zoom);
-        }
-        if(Gdx.input.isKeyPressed(Input.Keys.Q)) {
-            if (camera.zoom > 0.1)
-                camera.zoom -= 0.1;
-        }
-        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-            camera.translate(-13*(camera.zoom/3), 0, 0);
-        }
-        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-            camera.translate(13*(camera.zoom/3), 0, 0);
-        }
-        if(Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
-            camera.translate(0, -13*(camera.zoom/3), 0);
-        }
-        if(Gdx.input.isKeyPressed(Input.Keys.UP)) {
-            camera.translate(0, 13*(camera.zoom/3), 0);
-        }
-    }
-
-
     @Override
     public void resize(int width, int height) {
 
@@ -138,6 +123,100 @@ public class GameScreen implements Screen {
         floorTexture.dispose();
         wallTexture.dispose();
         doorTexture.dispose();
+    }
+
+    private void handlePressedKeys() {
+        for (Iterator<Integer> it=pressedKeys.iterator(); it.hasNext(); ) {
+            switch (it.next()) {
+
+                case Input.Keys.A:
+                    camera.zoom += 0.1;
+                    break;
+
+                case Input.Keys.Q:
+                    if (camera.zoom > 0.1)
+                        camera.zoom -= 0.1;
+                    break;
+
+                case Input.Keys.LEFT:
+                    camera.translate(-13*(camera.zoom/3), 0, 0);
+                    break;
+
+                case Input.Keys.RIGHT:
+                    camera.translate(13*(camera.zoom/3), 0, 0);
+                    break;
+
+                case Input.Keys.DOWN:
+                    camera.translate(0, -13*(camera.zoom/3), 0);
+                    break;
+
+                case Input.Keys.UP:
+                    camera.translate(0, 13*(camera.zoom/3), 0);
+                    break;
+            }
+        }
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        pressedKeys.add(keycode);
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        pressedKeys.remove(keycode);
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        if (button == Input.Buttons.LEFT) {
+            dragStartX = screenX;
+            dragStartY = screenY;
+        } else {
+            dragStartX = null;
+            dragStartY = null;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        if (dragStartX != null && dragStartY != null) {
+            int deltaX = screenX - dragStartX;
+            int deltaY = screenY - dragStartY;
+            camera.translate(-2*(camera.zoom/3)*deltaX, 2*(camera.zoom/3)*deltaY, 0);
+
+            dragStartX = screenX;
+            dragStartY = screenY;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        if (amount == 1) {
+            camera.zoom += 0.4;
+        } else {
+            camera.zoom -= 0.4;
+        }
+        return false;
     }
 
     //OrthographicCamera camera;
