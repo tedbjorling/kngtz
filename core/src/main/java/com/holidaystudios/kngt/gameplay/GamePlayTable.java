666182a6cffbf0450709faecd275c65552bfc574
Anton Persson <don.juanton@gmail.com>
2014-02-23 17:44:48 +0100

    Testing scene2d.

---
+++ core/src/main/java/com/holidaystudios/kngt/gameplay/GamePlayTable.java
@@ -0,0 +1,31 @@
+package com.holidaystudios.kngt.screens;
+
+import java.util.Iterator;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.TimeUtils;
+
+public class GamePlayTable extends Table {
+    public PlayerKnight playerKnight;
+
+    public GamePlayTable(int hbound, int vbound) {
+        setBounds(0, 0, hbound, vbound);
+        setClip(true);
+        playerKnight = new PlayerKnight(this);
+        addActor(playerKnight);
+    }
+
+    @Override
+    public void act(float delta) {
+        super.act(delta);
+    }
+
+    @Override
+    public void draw(SpriteBatch batch, float parentAlpha) {
+        batch.setColor(Color.WHITE);
+        super.draw(batch, parentAlpha);
+    }
+}
